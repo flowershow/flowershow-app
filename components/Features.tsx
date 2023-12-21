6cd0a92ee5829507c99bf744ac9ad7e348595e6a
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-12-21 13:00:06 +0100

    [home page][xs]: add id to features section

--- components/Features.tsx
+++ components/Features.tsx
@@ -15,7 +15,7 @@ export const Features: React.FC<Props> = ({ features }) => {
     return (
         <>
             <div className="prose dark:prose-invert mx-auto max-w-screen-xl px-4 py-6 lg:items-center text-center">
-                <h2>Features</h2>
+                <h2 id="features">Features</h2>
                 <p>
                     Here are some of the cool features that are currently supported by
                     Flowershow
