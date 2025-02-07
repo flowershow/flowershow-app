bdb0048a3e06763f79aaf31185a926bb530b9f26
Ola Rubaj <ola@yway.pl>
2025-02-07 01:05:08 +0100

    switch home with cloud page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,144 +1,79 @@
 import React from "react";
-import Link from "next/link";
 import { GetStaticProps, GetStaticPropsResult } from "next";
-import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
 
 import { Hero } from "@/components/Hero";
-import { GitHubIcon } from "@/components/icons/GitHubIcon"
+/* import { FeaturesTiles } from "@/components/FeatureTiles"; */
+import { CTASection } from "@/components/CTASection";
+import { Pricing } from "@/components/Pricing";
 import { Button } from "@/components/common/Button"
-import { Features } from "@/components/Features";
-import WhatIsFlowershow from "@/components/WhatIsFlowershow";
-import SelfPublishSteps from "@/components/SelfPublishSteps";
-/* import CloudPublishSteps from "@/components/CloudPublishSteps"; */
-import type { CustomAppProps } from "./_app";
+
+import type { CustomAppProps } from "../_app";
 
 
 type Props = CustomAppProps;
 
 const heroProps = {
-    title: "Create websites from Markdown",
-    subtitle: "Turn your markdown notes into an elegant website and tailor it to your needs.",
+    title: "Avoid the hassle and complexity of deploying yourself.",
+    subtitle: "With Flowershow Cloud you can turn your markdown into a website in a couple of clicks.",
     highlights: [
-        "Easy to use",
-        "Obsidian syntax compatible",
-        "Open-source"
+        "All of Flowershow goodness",
+        "Always up to date with latest Flowershow",
+        "Obsidian compatible",
+        "Run off GitHub",
     ],
     infoBadges: [
-        (<Link
-            href="/cloud"
-            className="rounded-full bg-secondary-500/10 px-3 py-1 text-sm leading-6 ring-1 ring-inset ring-secondary/20">
-            <span className="mr-1 bg-clip-text text-transparent font-bold bg-gradient-to-r from-sky-500 to-indigo-400">Flowershow Cloud</span>
-            is here 💐🎉 - get early access
-            <ArrowNarrowRightIcon className="inline ml-1 h-5 w-5" aria-hidden="true" />
-        </Link>),
-        /* (<Link
-*     href="https://github.com/datopian/obsidian-flowershow"
-*     className="rounded-full bg-secondary-500/10 px-3 py-1 text-sm font-medium leading-6 text-secondary ring-1 ring-inset ring-secondary/20">
-*     Just shipped: Obsidian Flowershow Plugin v1.0<span className="ml-2">🚀</span>
-* </Link>), */
-        /* (<span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
-*     <span>Just shipped v1.0</span>
-*     <ChevronRightIcon className="h-5 w-5 text-gray-500" aria-hidden="true" />
-* </span>)
-  */
+        (<h2 className="max-w-lg text-3xl sm:text-4xl font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-indigo-400 ">
+            Flowershow Cloud
+        </h2>)
     ],
     ctaButtons: [
-        (<Button href="/docs/publish-howto">
-            <span>Quick start</span>
+        (<Button target="_blank" href="https://cloud.flowershow.app">
+            <span>Get started for free</span>
         </Button>),
-        (<Button
-            href="https://github.com/datopian/flowershow"
-            variant="outline"
-        >
-            <GitHubIcon />
-            <span className="ml-3">Star on GitHub</span>
-        </Button>)
     ]
 }
 
-const features = [
-    {
-        title: "Custom dark and light themes",
-        description:
-            "Change the fonts and colors used throughout your website as well as switch between dark and light modes.",
-        link: "/docs/custom-theme",
-        imageSrc: "/images/theme.png",
-    },
-    {
-        title: "Table of contents",
-        description:
-            "You can add a table of contents to your markdown pages as well as adding a site-wide table of contents in a LHS sidebar to allow your users to easily navigate to other pages on your website.",
-        link: "/docs/site-wide-toc",
-        imageSrc: "/images/toc.png",
-    },
-    {
-        title: "Blog Support",
-        description:
-            "Blog document type for your blog posts. This way you'll be able to fetch and display them on any of your pages by using our BlogsList component (or you can create a custom one).",
-        link: "/docs/blog",
-        imageSrc: "/images/blog.png",
-    },
-    {
-        title: "Markdown, MDX syntax support",
-        description:
-            "Flowershow was designed with Obsidian users in mind, so it aims to fully support Obsidian syntax, including CommonMark, GitHub Flavoured Markdown and Obsidian extensions, like Wiki links. All of your Markdown files are parsed as MDX. This means you not only can write your content using good old Markdown, but also enrich it with dynamic visualizations, immersive user interactions and much more!",
-        link: "/docs/syntax",
-        imageSrc: "/images/obs_dark.png",
-    },
-    {
-        title: "Tailwind support",
-        description:
-            "Flowershow comes with built-in tailwind support on any markdown page for styling your content.",
-        link: "/docs/tailwind",
-        imageSrc: "/images/tw.png",
-    },
-    {
-        title: "Mermaid and MathJax support",
-        description:
-            "Display Mermaid diagrams within your notes as well as math exaptions, where you can use LaTeX notations to denote formulas.",
-        link: "/docs/mermaid",
-        imageSrc: "/images/mermaid.png",
-    },
-    {
-        title: "Full-text search",
-        description:
-            "Flowershow supports search functionality to deliver realtime results for content available on all your pages. Clicking the result will lead the user to the relevant page.",
-        link: "/docs/search",
-        imageSrc: "/images/search.png",
-    },
-];
+/* const features = [{
+*     imageSrc: "/images/sync.png",
+*     imageAlt: "",
+*     title: "Always up to date with the latest Flowershow",
+*     description: `Flowershow Cloud ensures you're always using the latest version of Flowershow template.
+* There's no need for manual updates.`
+* },
+* {
+*     imageSrc: "/images/bolt.png",
+*     imageAlt: "",
+*     title: "Instant publishing",
+*     description: `Your content goes live instantly thanks to server-side rendering.
+* No waiting for builds - updates to your markdown files are reflected immediately on your site.`
+* },
+* {
+*     imageSrc: "/images/star.png",
+*     imageAlt: "",
+*     title: "All of Flowershow goodness",
+*     description: `Flowershow Cloud uses the core Flowershow template to render your sites,
+* offering all its features that bring the best out of your content and present it in an elegant website layout.`
+* },
+* {
+*     imageSrc: "/images/github.png",
+*     imageAlt: "",
+*     title: "Run off-GitHub",
+*     description: `Flowershow Clour renders your markdown files off of your GitHub repository,
+* directly into a live site with no additional hosting steps.`
+* }
+* ] */
 
 export default function Home() {
     return (
         <main>
             <Hero {...heroProps} />
-            <WhatIsFlowershow />
-            <div className="py-10 sm:px-2 lg:relative lg:px-0" id="overview">
-                <div className="prose dark:prose-invert mx-auto max-w-6xl px-4 lg:max-w-6xl lg:px-8 xl:px-12">
-                    <h2 className="text-center">🚀 Now available as Obsidian plugin! 🚀</h2>
-                    <p>
-                        Flowershow is now available as an Obsidian plugin! This means you can now
-                        publish your digital garden directly from Obsidian, in just a few easy steps, without having to use the command line at all!
-                        <p>👉 See our <a href="/docs/publish-howto">self-publish howto</a> to get started.</p>
-                        PS: It's so good, the Flowershow CLI has been deprecated and will no longer be maintained 😎
-                        <p>🚧 TBD: Support for configuring Flowershow from the Obsidian settings UI.</p>
-                    </p>
-                </div>
-            </div>
-            <SelfPublishSteps />
-            {/* <CloudPublishSteps /> */}
-            <Features features={features} />
-            {/** Why the name? **/}
-            <div className="py-10 sm:px-2 lg:relative lg:px-0">
-                <div className="prose dark:prose-invert mx-auto max-w-2xl px-4 lg:max-w-4xl lg:px-8 xl:px-12">
-                    <h2 className="text-center">Why the name?</h2>
-                    <p>
-                        Flowershow is about sharing your digital garden -- putting it "on
-                        show" to the world. And what do you have in your garden? Flowers!
-                        Hence "Flowershow": it shows off your digital garden to the world!
-                    </p>
-                </div>
-            </div>
+            <Pricing />
+            <CTASection
+                title="Start using Flowershow Cloud now!"
+                subtitle="Try it for free and see how easy it is to turn your markdown into an elegant website."
+                cta="Get started for free"
+                href="https://cloud.flowershow.app"
+            />
         </main>
     );
 }
---
+++ pages/self-hosted/index.tsx
@@ -0,0 +1,160 @@
+import React from "react";
+import Link from "next/link";
+import { GetStaticProps, GetStaticPropsResult } from "next";
+import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
+
+import { Hero } from "@/components/Hero";
+import { GitHubIcon } from "@/components/icons/GitHubIcon"
+import { Button } from "@/components/common/Button"
+import { Features } from "@/components/Features";
+import WhatIsFlowershow from "@/components/WhatIsFlowershow";
+import SelfPublishSteps from "@/components/SelfPublishSteps";
+/* import CloudPublishSteps from "@/components/CloudPublishSteps"; */
+import type { CustomAppProps } from "../_app";
+
+
+type Props = CustomAppProps;
+
+const heroProps = {
+    title: "Create websites from Markdown",
+    subtitle: "Turn your markdown notes into an elegant website and tailor it to your needs.",
+    highlights: [
+        "Easy to use",
+        "Obsidian syntax compatible",
+        "Open-source"
+    ],
+    infoBadges: [
+        (<Link
+            href="/cloud"
+            className="rounded-full bg-secondary-500/10 px-3 py-1 text-sm leading-6 ring-1 ring-inset ring-secondary/20">
+            <span className="mr-1 bg-clip-text text-transparent font-bold bg-gradient-to-r from-sky-500 to-indigo-400">Flowershow Cloud</span>
+            is here 💐🎉 - get early access
+            <ArrowNarrowRightIcon className="inline ml-1 h-5 w-5" aria-hidden="true" />
+        </Link>),
+        /* (<Link
+*     href="https://github.com/datopian/obsidian-flowershow"
+*     className="rounded-full bg-secondary-500/10 px-3 py-1 text-sm font-medium leading-6 text-secondary ring-1 ring-inset ring-secondary/20">
+*     Just shipped: Obsidian Flowershow Plugin v1.0<span className="ml-2">🚀</span>
+* </Link>), */
+        /* (<span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
+*     <span>Just shipped v1.0</span>
+*     <ChevronRightIcon className="h-5 w-5 text-gray-500" aria-hidden="true" />
+* </span>)
+  */
+    ],
+    ctaButtons: [
+        (<Button href="/docs/publish-howto">
+            <span>Quick start</span>
+        </Button>),
+        (<Button
+            href="https://github.com/datopian/flowershow"
+            variant="outline"
+        >
+            <GitHubIcon />
+            <span className="ml-3">Star on GitHub</span>
+        </Button>)
+    ]
+}
+
+const features = [
+    {
+        title: "Custom dark and light themes",
+        description:
+            "Change the fonts and colors used throughout your website as well as switch between dark and light modes.",
+        link: "/docs/custom-theme",
+        imageSrc: "/images/theme.png",
+    },
+    {
+        title: "Table of contents",
+        description:
+            "You can add a table of contents to your markdown pages as well as adding a site-wide table of contents in a LHS sidebar to allow your users to easily navigate to other pages on your website.",
+        link: "/docs/site-wide-toc",
+        imageSrc: "/images/toc.png",
+    },
+    {
+        title: "Blog Support",
+        description:
+            "Blog document type for your blog posts. This way you'll be able to fetch and display them on any of your pages by using our BlogsList component (or you can create a custom one).",
+        link: "/docs/blog",
+        imageSrc: "/images/blog.png",
+    },
+    {
+        title: "Markdown, MDX syntax support",
+        description:
+            "Flowershow was designed with Obsidian users in mind, so it aims to fully support Obsidian syntax, including CommonMark, GitHub Flavoured Markdown and Obsidian extensions, like Wiki links. All of your Markdown files are parsed as MDX. This means you not only can write your content using good old Markdown, but also enrich it with dynamic visualizations, immersive user interactions and much more!",
+        link: "/docs/syntax",
+        imageSrc: "/images/obs_dark.png",
+    },
+    {
+        title: "Tailwind support",
+        description:
+            "Flowershow comes with built-in tailwind support on any markdown page for styling your content.",
+        link: "/docs/tailwind",
+        imageSrc: "/images/tw.png",
+    },
+    {
+        title: "Mermaid and MathJax support",
+        description:
+            "Display Mermaid diagrams within your notes as well as math exaptions, where you can use LaTeX notations to denote formulas.",
+        link: "/docs/mermaid",
+        imageSrc: "/images/mermaid.png",
+    },
+    {
+        title: "Full-text search",
+        description:
+            "Flowershow supports search functionality to deliver realtime results for content available on all your pages. Clicking the result will lead the user to the relevant page.",
+        link: "/docs/search",
+        imageSrc: "/images/search.png",
+    },
+];
+
+export default function Home() {
+    return (
+        <main>
+            <Hero {...heroProps} />
+            <WhatIsFlowershow />
+            <div className="py-10 sm:px-2 lg:relative lg:px-0" id="overview">
+                <div className="prose dark:prose-invert mx-auto max-w-6xl px-4 lg:max-w-6xl lg:px-8 xl:px-12">
+                    <h2 className="text-center">🚀 Now available as Obsidian plugin! 🚀</h2>
+                    <p>
+                        Flowershow is now available as an Obsidian plugin! This means you can now
+                        publish your digital garden directly from Obsidian, in just a few easy steps, without having to use the command line at all!
+                        <p>👉 See our <a href="/docs/publish-howto">self-publish howto</a> to get started.</p>
+                        PS: It's so good, the Flowershow CLI has been deprecated and will no longer be maintained 😎
+                        <p>🚧 TBD: Support for configuring Flowershow from the Obsidian settings UI.</p>
+                    </p>
+                </div>
+            </div>
+            <SelfPublishSteps />
+            {/* <CloudPublishSteps /> */}
+            <Features features={features} />
+            {/** Why the name? **/}
+            <div className="py-10 sm:px-2 lg:relative lg:px-0">
+                <div className="prose dark:prose-invert mx-auto max-w-2xl px-4 lg:max-w-4xl lg:px-8 xl:px-12">
+                    <h2 className="text-center">Why the name?</h2>
+                    <p>
+                        Flowershow is about sharing your digital garden -- putting it "on
+                        show" to the world. And what do you have in your garden? Flowers!
+                        Hence "Flowershow": it shows off your digital garden to the world!
+                    </p>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export const getStaticProps: GetStaticProps = async (): Promise<
+    GetStaticPropsResult<Props>
+> => {
+    return {
+        props: {
+            meta: {
+                urlPath: "/",
+                showToc: false,
+                showEditLink: false,
+                showSidebar: false,
+                showComments: false,
+            },
+        },
+    };
+};
