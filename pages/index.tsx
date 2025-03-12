8fd41db38cef2312e73901b84614cd3fc8cace45
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2025-03-12 14:25:27 +0100

    new features

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,26 +31,50 @@ const heroProps = {
 
 const features = [
     {
-        title: "Markdown, MDX syntax support",
+        title: "Markdown and Obsidian Syntax Support",
         description:
             "Flowershow was designed with Obsidian users in mind, so it aims to fully support Obsidian syntax, including CommonMark, GitHub Flavoured Markdown and Obsidian extensions, like Wiki links. All of your Markdown files are parsed as MDX. This means you not only can write your content using good old Markdown, but also enrich it with dynamic visualizations, immersive user interactions and much more!",
         link: "/docs/syntax",
-        imageSrc: "/images/obs_dark.png",
+        imageSrc: "/markdown.png",
     },
     {
         title: "Mermaid and MathJax support",
         description:
             "Display Mermaid diagrams within your notes as well as math exaptions, where you can use LaTeX notations to denote formulas.",
         link: "/docs/mermaid",
-        imageSrc: "/images/mermaid.png",
+        imageSrc: "/math_and_mermaid.png",
     },
     {
-        title: "Table of contents",
+        title: "Table of Contents and Sitemap",
         description:
             "You can add a table of contents to your markdown pages as well as adding a site-wide table of contents in a LHS sidebar to allow your users to easily navigate to other pages on your website.",
         link: "/docs/site-wide-toc",
-        imageSrc: "/images/toc.png",
-    }
+        imageSrc: "/toc_and_sitemap.png",
+    },
+    {
+        title: "Page Comments",
+        description: "Enable page comments powered by Giscus - a comments system based on GitHub Discussions.",
+        link: "/docs/site-wide-toc",
+        imageSrc: "/comments.png",
+    },
+    {
+        title: "Elegant Layout",
+        description: "Display your content in an elegant format supporting title, description, authors, publish date, featured image and more.",
+        link: "/docs/site-wide-toc",
+        imageSrc: "/layout.png",
+    },
+    {
+        title: "Hero Section",
+        description: "aaa",
+        link: "/docs/site-wide-toc",
+        imageSrc: "/hero.png",
+    },
+    // {
+    //     title: "Custom CSS",
+    //     description: "aaa",
+    //     link: "/docs/site-wide-toc",
+    //     imageSrc: "/tbd.png",
+    // },
 ];
 
 export default function Home() {
